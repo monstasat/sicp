e70c39e60ff529aafcef0abfd40f7b6bbbaba88e
monstasat <sashayanin@gmail.com>
2020-05-03 13:00:43 +0300

    chore: add exercise 1.10

--- chapter_1.scm
+++ chapter_1.scm
@@ -1,7 +1,9 @@
-;; Exercise 1.1
-;; Below is a sequence of expressions.
-;; What is the result printed by the interpreter in response to each expression?
-;; Assume that the sequence is to be evaluated in the order in which it is presented.
+; Exercise 1.1
+; Below is a sequence of expressions.
+; What is the result printed by the interpreter in response to each expression?
+; Assume that the sequence is to be evaluated in the order in which it is presented.
+;
+; Answer:
 
 10
 ; 10
@@ -51,9 +53,10 @@
 ; 16
 
 
-;; Exercise 1.2
-;; Translate the following expression into prefix form
-
+; Exercise 1.2
+; Translate the following expression into prefix form
+;
+; Answer:
 ; 5 + 4 + (2 - (3 - (6 + 4/5)))
 ; -----------------------------
 ; 3(6 - 2)(2 - 7)
@@ -63,10 +66,11 @@
    (* 3 (- 6 2) (- 2 7))
 )
 
-;; Exercise 1.3
-;; Define a procedure that takes three numbers as arguments
-;; and returns the sum of the squares of the two larger numbers.
 
+; Exercise 1.3
+; Define a procedure that takes three numbers as arguments
+; and returns the sum of the squares of the two larger numbers.
+;
 ; Answer
 (define (square x) (* x x))
 
@@ -80,75 +84,120 @@
   )
 )
 
-;; Exercise 1.4
-;; Observe that our model of evaluation allows for combinations whose operators are compound expressions.
-;; Use this observation to describe the behavior of the following procedure:
 
-;; (define (a-plus-abs-b a b)
-;;  ((if (> b 0) + -) a b))
-
-; Answer
+; Exercise 1.4
+; Observe that our model of evaluation allows for combinations whose operators are compound expressions.
+; Use this observation to describe the behavior of the following procedure:
+;
+; (define (a-plus-abs-b a b)
+;  ((if (> b 0) + -) a b))
+;
+; Answer:
 ; a-plus-abs-b returns a + b when b > 0 and a - b otherwise
 
-;; Exercise 1.5
-;; Ben Bitdiddle has invented a test to determine whether the interpreter
-;; he is faced with is using applicative-order evaluation or normal-order evaluation.
-;; He defines the following two procedures:
 
-;; (define (p) (p))
-
-;; (define (test x y)
-;;   (if (= x 0)
-;;       0
-;;       y))
-
-;; Then he evaluates the expression
-
-;; (test 0 (p))
-
-;; What behavior will Ben observe with an interpreter that uses applicative-order evaluation?
-;; What behavior will he observe with an interpreter that uses normal-order evaluation?
-;; Explain your answer. (Assume that the evaluation rule for the special form if is the same
-;; whether the interpreter is using normal or applicative order:
-;; The predicate expression is evaluated first, and the result determines whether to evaluate
-;; the consequent or the alternative expression.)
-
+; Exercise 1.5
+; Ben Bitdiddle has invented a test to determine whether the interpreter
+; he is faced with is using applicative-order evaluation or normal-order evaluation.
+; He defines the following two procedures:
+;
+; (define (p) (p))
+;
+; (define (test x y)
+;   (if (= x 0)
+;       0
+;       y))
+;
+; Then he evaluates the expression
+;
+; (test 0 (p))
+;
+; What behavior will Ben observe with an interpreter that uses applicative-order evaluation?
+; What behavior will he observe with an interpreter that uses normal-order evaluation?
+; Explain your answer. (Assume that the evaluation rule for the special form if is the same
+; whether the interpreter is using normal or applicative order:
+; The predicate expression is evaluated first, and the result determines whether to evaluate
+; the consequent or the alternative expression.)
+;
 ; Answer:
 ; when application-order evaluation is used, expression will return zero
 ; when normal-order evaluation is used, expression will never terminate, since
 ; (p) always resolves to itself.
 
-;; Exercise 1.6
-;; see https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-10.html#%_thm_1.6
 
+; Exercise 1.6
+; see https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-10.html#%_thm_1.6
+;
 ; Answer
 ; When 'if' special form is used, only expression which satisfies the predicate is evaluated.
 ; That is not true for 'new-if' procedure.
 ; In 'sqrt-iter', implemented with 'new-if', stack overflow will occur since 'sqrt-iter' will
 ; always recursively call itself regardless of predicate result.
 
-;; Exercise 1.7
 
+; Exercise 1.7
 ; TODO
 
-;; Exercise 1.8
 
+; Exercise 1.8
 ; TODO
 
-;; Exercise 1.9
-;; see https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-11.html#%_thm_1.9
-;; (define (+ a b)
-;;   (if (= a 0)
-;;       b
-;;       (inc (+ (dec a) b))))
 
-;; (define (+ a b)
-;;   (if (= a 0)
-;;       b
-;;       (+ (dec a) (inc b))))
-
+; Exercise 1.9
+; see https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-11.html#%_thm_1.9
+; (define (+ a b)
+;   (if (= a 0)
+;       b
+;       (inc (+ (dec a) b))))
+;
+; (define (+ a b)
+;   (if (= a 0)
+;       b
+;       (+ (dec a) (inc b))))
+;
 ; Answer
 ; first process is recursive, since '+' procedure needs to wait
 ; for the result of itself to finish calculation.
 ; second one is iterative, since all data needed for evaluation is available
 ; from the scope of the procedure (and from the global scope - procedures 'dec' and 'inc')
+
+
+; Exercise 1.10
+; see https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-11.html#%_thm_1.10
+; (define (A x y)
+;   (cond ((= y 0) 0)
+;         ((= x 0) (* 2 y))
+;         ((= y 1) 2)
+;         (else (A (- x 1)
+;                  (A x (- y 1))))))
+;
+; What are the values of the following expressions?
+; (A 1 10)
+;
+; (A 2 4)
+;
+; (A 3 3)
+;
+; Answer:
+; (A 1 10) - 2^10 = 1024
+; (A 2 4) - ... (A 1 16) = 2^16 = 65536
+; (A 3 3) - ... (A 1 16) = 2^16 = 65536
+;
+; Consider the following procedures, where A is the procedure defined above:
+;
+; (define (f n) (A 0 n))
+;
+; (define (g n) (A 1 n))
+;
+; (define (h n) (A 2 n))
+;
+; (define (k n) (* 5 n n))
+; 
+; Give concise mathematical definitions for the functions computed by the procedures f, g, and h 
+; for positive integer values of n. For example, (k n) computes 5n2.
+;
+; Answer:
+;
+; (define (f n) (A 0 n)) - evaluates (2 * n)
+; (define (g n) (A 1 n)) - evaluates 0 for n = 0, 2^n for n > 1
+; (define (h n) (A 2 n)) - evaluates 0 for n = 0, 2 for n = 1, 2^(2^n) for n > 1
